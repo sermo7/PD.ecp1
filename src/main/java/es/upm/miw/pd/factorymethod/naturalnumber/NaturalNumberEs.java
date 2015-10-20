812fba554234da96aeeaf80b326d31a5a57098d3
Sergio Morato <s.mvillar@alumnos.upm.es>
2015-10-20 12:36:25 +0200

    Creación NaturalNumber

---
+++ src/main/java/es/upm/miw/pd/factorymethod/naturalnumber/NaturalNumberEs.java
@@ -0,0 +1,33 @@
+package es.upm.miw.pd.factorymethod.naturalnumber;
+
+public class NaturalNumberEs {
+    private int value;
+
+    private static final String[] textValue = {"cero", "uno", "dos", "tres", "cuatro", "cinco"};
+
+    public NaturalNumberEs(int value) {
+        this.setValue(value);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        assert value >= 0;
+        this.value = value;
+    }
+
+    public void add(int value) {
+        this.setValue(this.value + value);
+    }
+
+    public String getTextValue() {
+        if (this.value < textValue.length) {
+            return NaturalNumberEs.textValue[this.value];
+        } else {
+            return "???";
+        }
+    }
+
+}
